b257d15e20f5ea92df6e5010a445252e5292f249
syotaro <m154030kdy@gmail.com>
2022-09-04 16:55:48 +0900

    map timescale

--- tabular-playground-series-sep-2022/visualize.py
+++ tabular-playground-series-sep-2022/visualize.py
@@ -1,7 +1,5 @@
 # %%
 # standard lib
-
-import json
 from dataclasses import dataclass
 from pathlib import Path
 # third party
@@ -9,7 +7,7 @@ import numpy as np
 import pandas as pd
 from plotly import express as px
 
-CREATE_PICTURE = False
+CREATE_IMAGE = True
 
 
 @dataclass
@@ -31,24 +29,108 @@ print(
     sep="\n"
 )
 # %%
-df = train_data.groupby("country", as_index=False).aggregate("sum")
-df["iso_code"] = ["BEL", "FRA", "DEU", "ITA", "POL", "ESP"]
+# location
+trans_isocode = {
+    'Belgium': "BEL",
+    'France': "FRA",
+    'Germany': "DEU",
+    'Italy': "ITA",
+    'Poland': "POL",
+    'Spain': "ESP"
+}
 fig = px.choropleth(
-    df,
+    train_data.replace(trans_isocode),
     color="num_sold",
-    locations="iso_code",
+    locations="country",
     scope="europe",
+    animation_frame="date",
+    range_color=[0, 1000]
 )
 fig.update_geos(
     # fitbounds="locations",
     visible=True,
     center={"lat": 48.0, "lon": 7.0},
-    projection_scale=2.8
+    projection_scale=2.8,
+    showland=True,
+    showocean=True,
+    landcolor="LightGreen",
+    oceancolor="LightBlue",
 )
 fig.update_layout(
     margin={"r": 0, "t": 0, "l": 0, "b": 0},
-    height=700,
+    height=700
 )
 fig.show()
+if CREATE_IMAGE:
+    fig = px.choropleth(
+        (
+            train_data
+            .groupby("country", as_index=False)
+            .median()
+            .replace(trans_isocode)
+        ),
+        color="num_sold",
+        locations="country",
+        scope="europe",
+        range_color=[0, 1000]
+    )
+    fig.update_geos(
+        # fitbounds="locations",
+        visible=True,
+        center={"lat": 48.0, "lon": 7.0},
+        projection_scale=2.8,
+        showland=True,
+        showocean=True,
+        landcolor="LightGreen",
+        oceancolor="LightBlue",
+    )
+    fig.update_layout(
+        margin={"r": 0, "t": 0, "l": 0, "b": 0},
+        height=700
+    )
+    fig.write_image(r"src\location_numsold.svg")
 
+# %%
+# time scale
+
+
+def timescale_lineplot(data: pd.DataFrame, title: str):
+    fig = px.line(
+        data,
+        x="date",
+        y="num_sold",
+        color="product",
+        facet_row="store"
+    )
+    fig.update_layout(
+        margin={"r": 10, "t": 10, "l": 10, "b": 40},
+        width=800,
+        height=600,
+        legend={
+            "yanchor": "top",
+            "y": 0.48,
+            "xanchor": "left",
+            "x": 0.01
+        },
+        title={
+            "text": title,
+            "font": {"size": 22, "color": "black"},
+            "x": 0.95,
+            "y": 0.99,
+        },
+        xaxis={
+            "rangeslider": {"visible": True, "thickness": 0.01},
+            "type": "date"
+        }
+    )
+    if CREATE_IMAGE:
+        fig.write_image(rf"src\timescale_{title}.svg")
+    fig.show()
+
+
+for country in train_data["country"].unique():
+    timescale_lineplot(
+        train_data.query(f"country=='{country}'"),
+        country
+    )
 # %%
